fd4d9c5173bf863b55c0a6dbe63f671b6086e055
osamu <oosamuuy@gmail.com>
2017-10-29 18:47:12 +0900

    setup

---
+++ approot/optimazer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# optimazer
+php composer.phar install
+php composer.phar dump-autoload
+php artisan clear-compiled
+php artisan optimize
+php artisan config:cache
+php artisan route:cache
+php artisan view:clear
+rm -f bootstrap/cache/config.php
+
+# setting
+php artisan storage:link
---
+++ approot/setup.sh
@@ -0,0 +1,8 @@
+chmod 777 bootstrap/cache
+chmod 777 storage/app/public
+chmod 777 storage/framework/cache
+chmod 777 storage/framework/sessions
+chmod 777 storage/framework/views
+chmod 777 storage/logs
+chmod 666 storage/logs/laravel.log
+chmod -R 777 storage
