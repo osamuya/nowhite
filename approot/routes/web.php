ca2957dc1d0651be5fe55df0c3b04bf72110a2d9
osamu yamakami <osamuyamakami@osamunoMacBook-puro.local>
2017-11-23 16:38:29 +0900

    画像セピア

--- approot/routes/web.php
+++ approot/routes/web.php
@@ -12,15 +12,17 @@
 */
 
 Route::match(['get', 'post'], '/', 'TopController@index');
+//Route::get('/signup', 'SignupController@index');
+
 
 // local only for testing or view
 if (env("APP_ENV")=="local" || env("APP_ENV")=="develop") {
-	Route::get('/list', function () { return view('mock/list'); });
-	Route::get('/mock', function () { return view('mock/index'); });
-	Route::get('/mock/blog_archives', function () { return view('mock/blog_archives'); });
-	Route::get('/mock/mixed_blood', function () { return view('mock/mixed_blood'); });
-	Route::get('/mock/blog_top', function () { return view('mock/blog_top'); });
-	Route::get('/mock/blog_single', function () { return view('mock/blog_single'); });
+    Route::get('/list', function () { return view('mock/list'); });
+    Route::get('/mock', function () { return view('mock/index'); });
+    Route::get('/mock/blog_archives', function () { return view('mock/blog_archives'); });
+    Route::get('/mock/mixed_blood', function () { return view('mock/mixed_blood'); });
+    Route::get('/mock/blog_top', function () { return view('mock/blog_top'); });
+    Route::get('/mock/blog_single', function () { return view('mock/blog_single'); });
 }
 
 /*
